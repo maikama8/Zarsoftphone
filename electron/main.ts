d1c0888b00cba8dc070fd148154a6f91a7f6b3cb
maikama8 <50441825+maikama8@users.noreply.github.com>
2026-04-10 09:23:30 +0100

    Disable automatic DevTools opening on app launch

--- electron/main.ts
+++ electron/main.ts
@@ -52,7 +52,8 @@ function createWindow() {
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173')
-    mainWindow.webContents.openDevTools()
+    // DevTools can be opened manually with Cmd+Option+I (macOS) or Ctrl+Shift+I (Windows/Linux)
+    // mainWindow.webContents.openDevTools()
   } else {
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
   }
